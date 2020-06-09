13453d2d649ec2e35a9cb44f041e6003a7659b97
David Fisher <fisherds@rose-hulman.edu>
2020-06-09 16:44:51 -0400

    Finished RgbJoystick project

--- FollowAlongs/LearningC/RGBJoystick/RGBJoystick.ino
+++ FollowAlongs/LearningC/RGBJoystick/RGBJoystick.ino
@@ -1,19 +1,50 @@
+int rPot = 0;
+int bPot = 0;
+
 void setup() {
   Serial.begin(9600); // Com speed.  bits per second
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
+  
+  pinMode(2, INPUT_PULLUP);  // Add the pull up resistor.
+  
+  // Example FOR loop
+  
+  for (int k = 0; k < 5; k++) {
+    Serial.println(k);
+    delay(500);
+  }
+  
 }
 
 void loop() {
-  red();
-  delay(1000);
+  rPot = analogRead(0);
+  Serial.print(" R=");
+  Serial.print(rPot);
+  
+  bPot = analogRead(1);
+  Serial.print(" B=");
+  Serial.println(bPot);
+  
+
+  //digitalWrite(11, HIGH);
+//  red();
+//  delay(1000);
 //  green();
 //  delay(1000);
-  off();
-  delay(1000);
-    
+//  off();
+//  delay(1000);
+  
+  
   //rgb(61, 217, 212);
+  if ( digitalRead(2) ) {
+    rgb(rPot/4, 0, bPot/4);
+  } else {
+    off();
+  }
+  
+  delay(500);
 }
 
 void rgb(int r, int g, int b) {
@@ -22,6 +53,8 @@ void rgb(int r, int g, int b) {
   analogWrite(10, g);
   analogWrite(11, b);
 }
+
+
 
 void red() {
   analogWrite(9, 255);
